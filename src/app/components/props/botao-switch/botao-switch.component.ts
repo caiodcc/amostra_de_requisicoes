94b5f5a8910f661127d0afbf6084d8dcce8d314d
caiodcc <caio.dcc.marques@gmail.com>
2023-03-16 13:23:30 -0300

    Versão 0

---
+++ src/app/components/props/botao-switch/botao-switch.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { CalcComponent } from '../../calc/calc.component';
+
+
+@Component({
+  selector: 'app-botao-switch',
+  templateUrl: './botao-switch.component.html',
+  styleUrls: ['./botao-switch.component.css']
+})
+export class BotaoSwitchComponent extends CalcComponent {
+
+  
+  
+
+
+}
